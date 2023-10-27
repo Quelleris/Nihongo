5c758aa6ebe0f457300c11fe373b6e16435851e4
Quelleris <74406524+Quelleris@users.noreply.github.com>
2023-10-27 21:57:56 +0200

    Add hiragana list

--- Kanji-app/src/pages/HiraganaPage.tsx
+++ Kanji-app/src/pages/HiraganaPage.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import hiragana from "../data/hiragana.json";
 
 export const HiraganaPage = () => {
-  return <div>HiraganaPage</div>;
+  return (
+    <section className="section">
+      <div className="container">
+        <h1>Hiragana</h1>
+        <div className="grid-wrapper">
+          <ul className="hiragana-list">
+            {hiragana.map((hiragana) => {
+              return (
+                <li className="kana-card" key={hiragana.kana}>
+                  <p className="kana-kana">{hiragana.kana}</p>
+                  <p className="kana-roumanji">{hiragana.roumaji}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
